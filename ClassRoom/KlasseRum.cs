ed2d4b3359caedbba8f1f3b1e3c74d4afe0754e6
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2018-01-29 13:21:47 +0100

    KlassRum in progess

--- ClassRoom/KlasseRum.cs
+++ ClassRoom/KlasseRum.cs
@@ -8,9 +8,17 @@ namespace ClassRoom
 {
     public class KlasseRum
     {
+        private string _klasse;
+        private List<Studerende> _klasseListe;
+        private DateTime _semesterStart;
+
+
+        
         public string Klasse { get; set; }
         public List<Studerende> Klassliste { get; set; }
         public DateTime SemesterStart { get; set; }
+
+       
 
         public KlasseRum()
         {
