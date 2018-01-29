8cefc6e605b03dc93bf3e920f86791c53037e055
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2018-01-29 13:01:51 +0100

    Class  Studerende” done

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+   public class Studerende
+    {
+        public string Navn { get; set; }
+        public int Fødselsmåned { get; set; }
+
+        public int Fødselmåned { get; set; }
+
+        public Studerende(string navn, int fødselsmåned, int fødselmåned)
+        {
+            Navn = navn;
+            Fødselsmåned = fødselsmåned;
+            Fødselmåned = fødselmåned;
+        }
+    }
+}
